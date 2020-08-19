a6945039a15d0d7767a08c0df3169617faae96ea
MrSayyod <ssayyod@gmail.com>
2020-08-19 18:03:27 +0500

    Create 'Friendship' model, add 'friends', 'pending_requests', 'friend_requests' and 'confirm_friendship' methods to 'User' model

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,27 @@ class User < ApplicationRecord
   has_many :posts
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+  has_many :friendships, dependent: :destroy
+  has_many :reverse_friendships, class_name: "Friendship", foreign_key: "friend_id"
+
+  def friends
+    friends_array = friendships.map { | friendship | friendship.friend if friendship.status }
+    reverse_friends_array = friendships.map { | friendship | friendship.user if friendship.status }
+    all_friends_array = friends_array + reverse_friends_array
+    all_friends_array.compact
+  end
+
+  def pending_requests
+    friendships.map { | friendship | friendship.friend unless friendship.status }.compact
+  end
+
+  def friend_requests
+    friendships.map { | friendship | friendship.user unless friendship.status }.compact
+  end
+  
+  def confirm_friendship(user)
+    friendship = reverse_friendships.find { | f | f.user == user}
+    friendship.status = true
+    friendship.save
+  end
 end
