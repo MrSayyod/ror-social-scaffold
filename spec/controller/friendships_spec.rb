c37fdcadc6d91aada7fca906a72b3d7055ee56a6
MrSayyod <ssayyod@gmail.com>
2020-08-24 21:42:08 +0500

    Fix rubocop errors

--- spec/controller/friendships_spec.rb
+++ spec/controller/friendships_spec.rb
@@ -25,7 +25,27 @@ RSpec.feature 'Friendships', type: :feature do
     click_on('Send Friend Request')
     expect(page).to have_content('You have sent a friend request successfully!')
   end
+end
 
+RSpec.feature 'Friendships', type: :feature do
+  let(:user1) do
+    User.create(name: 'User', email: 'user@example.com', password: '123456', password_confirmation: '123456')
+  end
+  let(:user2) do
+    User.create(name: 'Friend', email: 'friend@example.com', password: '123456', password_confirmation: '123456')
+  end
+
+  before :each do
+    user1
+    user2
+    visit 'users/sign_in'
+    within('#new_user') do
+      fill_in 'user_email', with: 'user@example.com'
+      fill_in 'user_password', with: '123456'
+    end
+    click_button 'Log in'
+    visit '/users'
+  end
   it 'should let the user accept a friend request' do
     click_on('Send Friend Request')
     click_on 'Sign out'
